fdb933ce3062ed80a712e3e70727f9395ff3eb49
Jefté <jefte_lima@hotmail.com>
2024-08-13 10:47:11 -0300

    feat: add color pallet to the theme
    - Add color pallet in file  the colors.ts in the defaults of the theme
    - Import and set colors to the theme on theme.ts
    - Use the color in the home page to check its funcionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,19 @@ import Feed from "@/components/ui/Feed";
 import Footer from "@/components/ui/Footer";
 import Menu from "@/components/ui/Menu";
 import Text from "@/components/ui/Text";
+import { useTheme } from "@/theme/ThemeProvider";
 
 export default function Home() {
+  const theme = useTheme();
+
   return (
     <>
-      <Box tag="main">
+      <Box
+        tag="main"
+        styleSheet={{
+          backgroundColor: theme.colors.positive.x100,
+        }}
+      >
         <Background />
         <Menu />
         <Feed>
