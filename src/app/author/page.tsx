6152598bf3a30c013e1e9680870ffa55580d736f
Jefté Lima <jefte_lima@hotmail.com>
2024-11-15 16:33:38 -0300

    feat: reorganize page layout components
    - Update main layout fonts and refactor metadata
    - Update main page adding the new Header component and refactoring the layout
    - Add new author page component

--- src/app/author/page.tsx
+++ src/app/author/page.tsx
@@ -3,31 +3,30 @@
 import Background from '@/components/ui/Background'
 import Box from '@/components/ui/Box'
 import Feed from '@/components/ui/feed/Feed'
+import FeedHeader from '@/components/ui/feed/FeedHeader'
 import FeedPosts from '@/components/ui/feed/FeedPosts'
 import Footer from '@/components/ui/Footer'
-import Header from '@/components/ui/Header'
+import Menu from '@/components/ui/Menu'
 import { useTheme } from '@/hooks/useTheme'
 
 export default function HomePage() {
 	const theme = useTheme()
 
 	return (
-		<>
-			<Header />
-			<Box
-				tag='main'
-				styleSheet={{
-					backgroundColor: theme.colors.neutral.x000,
-					flex: 1,
-					alignItems: 'center',
-				}}
-			>
-				<Background />
-				<Feed>
-					<FeedPosts />
-				</Feed>
-				<Footer />
-			</Box>
-		</>
+		<Box
+			tag='main'
+			styleSheet={{
+				backgroundColor: theme.colors.neutral.x000,
+				flex: 1,
+				alignItems: 'center',
+			}}
+		>
+			<Background />
+			<Feed>
+				<FeedHeader />
+				<FeedPosts />
+			</Feed>
+			<Footer />
+		</Box>
 	)
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,35 +3,31 @@ import StyledComponentsRegistry from '@/lib/registry'
 import TemplateConfigProvider from '@/providers/TemplateConfigProvider'
 import ThemeProvider from '@/providers/ThemeProvider'
 import GlobalStyle from '@/theme/GlobalStyle'
-import type { Metadata, ResolvingMetadata } from 'next'
-import { Open_Sans } from 'next/font/google'
+import type { Metadata } from 'next'
+import { Cairo, Orbitron } from 'next/font/google'
 
-const openSans = Open_Sans({ subsets: ['latin'] })
+const orbitron = Orbitron({
+	subsets: ['latin'],
+	variable: '--font-orbitron',
+})
 
-type Params = {
-	params: Record<string, string>
-}
+const cairo = Cairo({
+	subsets: ['latin'],
+})
 
-export async function generateMetadata(
-	{ params }: Params,
-	parent: ResolvingMetadata,
-): Promise<Metadata> {
+export async function generateMetadata(): Promise<Metadata> {
 	const template = await templateConfig()
 
 	return {
 		title: template.site?.title || 'Blog page - Timeline',
-		description: template.site?.description || 'Welcome to the blog timeline',
+		description: template.site?.description || 'Welcome to the blog feel free to explore our posts',
 	}
 }
 
-export default function RootLayout({
-	children,
-}: Readonly<{
-	children: React.ReactNode
-}>) {
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
 	return (
 		<html lang='en'>
-			<body className={openSans.className}>
+			<body className={`${cairo.className} ${orbitron.variable}`}>
 				<StyledComponentsRegistry>
 					<TemplateConfigProvider>
 						<ThemeProvider>
