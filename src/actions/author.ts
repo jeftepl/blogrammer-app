c8e050e3e72c0c0718353896bf3fd5b912d1e2f7
Jefté Lima <jefte_lima@hotmail.com>
2024-11-21 18:39:26 -0300

    feat: add get all authors fetch functionality
    - Add authors function to fetch all authors
    - Implement useAuthors custom hook for all authors data management

--- src/actions/author.ts
+++ src/actions/author.ts
@@ -5,7 +5,7 @@ import fs from 'fs/promises'
 import matter from 'gray-matter'
 import path from 'path'
 
-export default async function author(id: string): Promise<Author | null> {
+export async function author(id: string): Promise<Author | null> {
 	const PATH_AUTHORS = path.resolve('.', '_data', 'authors')
 	const authorsFiles = await fs.readdir(PATH_AUTHORS, { encoding: 'utf-8' })
 
@@ -25,4 +25,25 @@ export default async function author(id: string): Promise<Author | null> {
 	}
 
 	return null
+}
+
+export async function authors(): Promise<Author[]> {
+	const PATH_AUTHORS = path.resolve('.', '_data', 'authors')
+	const authorsFiles = await fs.readdir(PATH_AUTHORS, { encoding: 'utf-8' })
+	const authors = []
+
+	for (const authorFile of authorsFiles) {
+		const authorPath = path.join(PATH_AUTHORS, authorFile)
+		const authorContent = await fs.readFile(authorPath, { encoding: 'utf-8' })
+		const { data, content } = matter(authorContent)
+
+		const regex = /^#\s+(.+)$/m
+		const titleMatch = content.match(regex)
+		const name = titleMatch ? titleMatch[1].trim() : ''
+		const description = content.replace(regex, '').trim()
+
+		authors.push({ ...data, name, description } as Author)
+	}
+
+	return authors
 }
--- src/hooks/useAuthor.ts
+++ src/hooks/useAuthor.ts
@@ -1,8 +1,8 @@
-import author from '@/actions/author'
+import { author, authors } from '@/actions/author'
 import { Author } from '@/types/Author'
 import { useEffect, useState } from 'react'
 
-export default function useAuthor(id: string) {
+export function useAuthor(id: string) {
 	const [data, setData] = useState<Author | null>(null)
 
 	useEffect(() => {
@@ -16,6 +16,24 @@ export default function useAuthor(id: string) {
 		}
 		fetchData()
 	}, [id])
+
+	return data
+}
+
+export function useAuthors() {
+	const [data, setData] = useState<Author[]>([])
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await authors()
+				setData(response)
+			} catch (err) {
+				console.error(err)
+			}
+		}
+		fetchData()
+	}, [])
 
 	return data
 }
