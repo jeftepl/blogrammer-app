212c1b70aa7e826ff5b01d0d9259759306acf954
jeftepl <jefte_lima@hotmail.com>
2025-02-19 07:12:28 -0300

    refactor(config): update template config yaml file
     - Move config.yaml to src directory
     - Update template config path in templateConfig action

--- src/actions/template.ts
+++ src/actions/template.ts
@@ -5,7 +5,7 @@ import path from 'path'
 import readYamlFile from 'read-yaml-file'
 
 export default async function templateConfig() {
-	const PATH_TEMPLATE_CONFIG = path.resolve('.', 'config.yaml')
+	const PATH_TEMPLATE_CONFIG = path.join(process.cwd(), 'src', 'config.yaml')
 	const templateConfig = await readYamlFile<TemplateConfig>(PATH_TEMPLATE_CONFIG)
 
 	return templateConfig
